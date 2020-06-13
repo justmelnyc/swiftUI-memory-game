da5e88ca72e1402a3b645266d88fe5a7040bcc90
yixuannliu <yixuann.liu.utoronto@gmail.com>
2020-06-13 18:15:50 -0400

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Cosmos Liu on 2020-06-13.
+//  Copyright © 2020 Cosmos Liu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
