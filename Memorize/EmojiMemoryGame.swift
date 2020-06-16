0aae8d36fd1faf34035806059fa83840c20dacc3
yixuannliu <yixuann.liu.utoronto@gmail.com>
2020-06-15 22:02:46 -0400

    Init

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,31 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Cosmos Liu on 2020-06-14.
+//  Copyright © 2020 Cosmos Liu. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var gameModel: MemoryGame<String> = createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["🥰", "😃", "😥"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) {pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+        
+    
+    // MARK: - Access to the Model
+    var cards: Array<MemoryGame<String>.Card> {
+        gameModel.cards
+    }
+    
+    // MARK: - Intents(s)
+    func choose(card: MemoryGame<String>.Card) {
+        gameModel.choose(card: card)
+    }
+}
