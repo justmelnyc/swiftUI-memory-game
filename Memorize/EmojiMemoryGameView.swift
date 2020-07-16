e349552b5ddd47dacc9f543231805708d8a879af
yixuannliu <yixuann.liu.utoronto@gmail.com>
2020-07-16 17:14:07 -0400

    geometry reader

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -19,9 +19,8 @@ struct EmojiMemoryGameView: View {
             }
         }
         .padding()
-        .aspectRatio(0.67, contentMode: .fit)
         .foregroundColor(Color.orange)
-        .font(viewModel.cards.count<10 ? .largeTitle : .title)
+        
     }
 }
 
@@ -29,16 +28,30 @@ struct CardView: View{
     var card: MemoryGame<String>.Card
     
     var body: some View {
+        GeometryReader(content: { geometry in
+            self.body(for: geometry.size)
+        })
+    }
+    
+    func body(for size: CGSize) -> some View {
         ZStack {
             if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3 )
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth )
                 Text(card.content)
             } else{
-                RoundedRectangle(cornerRadius: 10.0).fill()
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
             }
-                       
         }
+        .font(Font.system(size: fontSize(for: size)))
+    }
+    
+    // MARK: - Drawing Constants
+    
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3
+    func fontSize(for size: CGSize) -> CGFloat {
+        min(size.width, size.height) * 0.75
     }
 }
 
